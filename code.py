bb3ac4a8bedd41090939891c70f0a0beca4157bd
Naruvadi <AL58085@ad.wellpoint.com>
2023-12-05 19:13:07 +0530

    Commiting code and example

---
+++ code.py
@@ -0,0 +1,7 @@
+for a in range(1,101):
+    count ==0
+    for b in range (1,a+1):
+        if a%b ==0:
+            count++=1
+if count == 2:
+    print(a)
---
+++ prime.py
@@ -0,0 +1,10 @@
+num =7
+
+for i in range(2,num):
+    if num % i == 0:
+         print ("not a prime")
+    break
+else:
+    print("prime")
+
+   
